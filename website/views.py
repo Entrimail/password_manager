435a0df313032a404ba7f91a384cd3c4e718689e
Entrimail <matshe04@gmail.com>
2023-03-18 13:42:42 +0300

    modified files

--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import  login_required, current_user
 from .models import db, Note
-from werkzeug.security import generate_password_hash
 import json
+
 
 views = Blueprint('views', __name__)
 
