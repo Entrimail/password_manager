f61b0a34d881789c96ddd427f9d277113cfc10cc
Entrimail <matshe04@gmail.com>
2023-04-05 17:01:29 +0300

    Initial commit

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
 from flask_login import login_required, current_user
 from .models import db, Note
 import json
@@ -12,26 +12,34 @@ views = Blueprint('views', __name__)
 def home():
     if request.method == 'POST':
         name = request.form.get('name')
-        username = request.form.get('username') 
+        username = request.form.get('username')
         password = request.form.get('password')
         note = Note.query.filter_by(name=name, username=username, password=password).first()
         if note:
             flash('Note already exist', category='e')
         elif len(name) < 4 or len(username) < 4 or len(password) < 4:
             flash('Note is too short', category='e')
+            return redirect(url_for('views.home'))
         else:
-            new_note = Note(name=name, username=username, password=password, user_id=current_user.id)
-            db.session.add(new_note)
-            db.session.commit()      
-            flash('Note added!', category='s')
+            existing_note = Note.query.filter_by(name=name, username=username, password=password,
+                                                 user_id=current_user.id).first()
+            if existing_note:
+                flash('Note already exist', category='e')
+            else:
+                new_note = Note(name=name, username=username, password=password, user_id=current_user.id)
+                db.session.add(new_note)
+                db.session.commit()
+                flash('Note added!', category='s')
+                return redirect(url_for('views.home'))
+
     return render_template('home.html', user=current_user)
 
 
 @views.route('/delete-note', methods=['POST'])
 def delete_note():
     note = json.loads(request.data)
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
+    note_id = note['noteId']
+    note = Note.query.get(note_id)
     if note:
         if note.user_id == current_user.id:
             db.session.delete(note)
